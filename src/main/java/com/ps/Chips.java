7db926b067709613f2b47d7f1b2241baa3c022dd
Syed Haider <alihaidersyed101@gmail.com>
2024-05-26 14:05:37 -0400

    ignore .idea/workspace.xml. Added Chips class

---
+++ src/main/java/com/ps/Chips.java
@@ -0,0 +1,13 @@
+package com.ps;
+
+public class Chips extends Product{
+    private Sizes size;
+
+    public Sizes getSize() {
+        return size;
+    }
+
+    public void setSize(Sizes size) {
+        this.size = size;
+    }
+}
